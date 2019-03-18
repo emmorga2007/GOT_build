59e26bc99b463cea511c98fef7c81b6f2ec63c7c
Van Rys <tvanrys@tss-hr420-dmpd.local>
2019-03-18 16:00:02 -0400

    animating banner

--- js/GOT_master.js
+++ js/GOT_master.js
@@ -4,10 +4,25 @@
 
 	const 	sigils 		= document.querySelectorAll('.sigil-container'),
 			lightbox 	= document.querySelector('.lightbox'),
-			video		= document.querySelector('video');
+			video		= document.querySelector('video'),
+			closeLB 	= document.querySelector('.lightbox-close'),
+			banners 	= document.querySelector("#houseImages");
 
 	function openLightbox() {
+		// grab the right video source
+		// debugger;
+		// get the lowercase house name from the class list
+		let targetHouse = this.className.split(" ")[1];
+
+		// make sure the names match - needs to be uppercase
+		// stark becomes Stark -> first make a capital S, then add ark (or any house name)
+		let targetSrc = targetHouse.charAt(0).toUpperCase() + targetHouse.slice(1);
+
+		video.src = `video/House-${targetSrc}.mp4`;
+
 		lightbox.classList.add('lightbox-on');
+
+		video.load();
 		video.play();
 	}
 
@@ -19,7 +34,20 @@
 		video.pause();
 	}
 
-	sigils.forEach(sigil => sigil.addEventListener('click', openLightbox));
+	function animateBanner() {
+		const offSet = 600; // this is the offset / width of one image
+
+		// this is the total distance the images need to move as a pixel value
+		// dataset.offset is coming from each shield we click on 
+		totalOffset = this.dataset.offset * offSet + "px";
+
+		// set the style (CSS will animate this for us)
+		banners.style.right = totalOffset;
+	}
+
+	//sigils.forEach(sigil => sigil.addEventListener('click', openLightbox));
+	sigils.forEach(sigil => sigil.addEventListener('click', animateBanner));
 
 	video.addEventListener('ended', closeLightbox);
+	closeLB.addEventListener('click', closeLightbox);
 })();
