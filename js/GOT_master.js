e2e49fd9643a9149a58d68f2f7f3151d653266e5
Van Rys <tvanrys@tss-hr420-dmpd.local>
2019-03-18 14:25:40 -0400

    fixed video player bug

--- js/GOT_master.js
+++ js/GOT_master.js
@@ -8,10 +8,15 @@
 
 	function openLightbox() {
 		lightbox.classList.add('lightbox-on');
+		video.play();
 	}
 
 	function closeLightbox() {
 		lightbox.classList.remove('lightbox-on');
+		// rewind the video to the beginning
+		// and also pause it
+		video.currentTime = 0;
+		video.pause();
 	}
 
 	sigils.forEach(sigil => sigil.addEventListener('click', openLightbox));
